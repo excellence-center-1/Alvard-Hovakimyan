cdea8e708b2ee3b1f028503197d2bed1135f36a3
excellence-center-1 <117776971+excellence-center-1@users.noreply.github.com>
2022-11-10 07:26:41 -0500

    Update selsort.cpp
    changes code stile

--- Algorithms/selsort.cpp
+++ Algorithms/selsort.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 #include<iostream>
-void poxel(int *a1, int *a2)
-{
+void poxel(int *a1, int *a2){
 	int temp = *a1;
 	*a1 = *a2;
 	*a2 = temp;
 }
-void sort(int arr[], int n)
-{
+void sort(int arr[], int n){
 	int i, j, minidx;
 	for (i = 0; i < n-1; i++)
 	{
@@ -20,15 +18,13 @@ void sort(int arr[], int n)
 			poxel(&arr[minidx], &arr[i]);
 	}
 }
-void tpel(int arr[], int chap)
-{
+void tpel(int arr[], int chap){
 	int i;
 	for (i=0; i < chap; i++)
 		std::cout << arr[i] << " ";
     std::cout<<std::endl;
 }
-int main()
-{
+int main(){
 	int arr[] = {64, 25, 2, 55, 5};
 	int n = 5;
 	sort(arr, n);
